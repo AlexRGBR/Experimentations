8f0ca4d3c78f1649bc6efc8702752bca01be2bc2
AlexR <alex.richaume1@gmail.com>
2026-09-23 23:57:36 +0200

    feat(KDA): add GatedDeltaNet first version

--- src/experimentations/KDA/layers/deltanet.py
+++ src/experimentations/KDA/layers/deltanet.py
@@ -20,9 +20,37 @@ class RecurrentDeltaNet(nn.module):
         k_t = self.K(x)
         v_t = self.V(x)
 
-        
         mem_update = torch.outer(k_t, v_t)
         new_mem = current_mem + mem_update
+        output = q_t @ new_mem
+        return output, new_mem
+
+
+class RecurrentGatedDeltaNet(nn.module):
+
+    def __init__(self, hidden_dimension, model_dimension):
+        super().__init__()
+
+        self.hidden_dimension = hidden_dimension
+        self.model_dimension = model_dimension
+
+        self.Q = nn.Linear(model_dimension, hidden_dimension, bias=False)
+        self.K = nn.Linear(model_dimension, hidden_dimension, bias=False)
+        self.V = nn.Linear(hidden_dimension, hidden_dimension, bias=False)
+
+        self.alpha_projection = nn.Linear(model_dimension, 1)
+        self.exp_bias = nn.Parameter(torch.randn(1))
+        self.softplus = nn.Softplus()
+
+    def forward(self, x, current_mem):
+        q_t = self.Q(x)
+        k_t = self.K(x)
+        v_t = self.V(x)
+
+        alpha_t = -np.exp(self.exp_bias)*self.softplus(self.alpha_projection(x))
+
+        mem_update = torch.outer(k_t, v_t)
+        new_mem = alpha_t*current_mem + mem_update
         output = q_t @ new_mem
         return output, new_mem
 
